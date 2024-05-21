3d1a08ff53c055fd0dc0bd7b8235fee445c9756e
DanielW98 <wagner.daniel98@yahoo.com>
2024-05-21 14:29:12 +0300

    Lab02: Adaugat fisiere pentru exercitiul Vector2

---
+++ l02/exercitii/main.cpp
@@ -0,0 +1,17 @@
+#include "Vector2.h"
+// teoretic, nu trebuie sa includem iostream, ca Vector2.h il include dar
+// include guards (#ifndef #define #endif), ne fac sa nu fie inclus multiplu
+#include <iostream>
+// fara using namespace std;
+
+int main()
+{
+    // Aici codul vostru
+    // Vector2 v = Vector2(10, 20);
+    // Vector2 v2 = v + v;
+    // Vector2 v3 = -v;
+    // std::cout.operator<<(10);
+    // std::cout << v2 << std::endl;
+    // ilustrare functionalitati existente + implementate de voi
+    return 0;
+}
